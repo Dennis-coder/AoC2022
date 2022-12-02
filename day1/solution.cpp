93d3f9bc8ebce3e5dc47cb44e8271e344fd9e681
Dennis Christensen <dennis.f.christensen@gmail.com>
2022-12-02 07:45:43 +0100

    day1 cpp

--- day1/solution.cpp
+++ day1/solution.cpp
@@ -1,12 +1,38 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
+
 using namespace std;
 
+bool descending (int i,int j) { return (i>j); }
+
 int main () {
-    string temp;
+    string line;
+    vector<int> elfs;
+    int elf = 0;
     ifstream dataFile("indata.txt");
-    while (getline(dataFile, temp)) {
-        cout << temp;
+    
+    while (getline(dataFile, line)) {
+        if (!line.empty()) {
+            elf = elf + stoi(line);
+        } else {
+            elfs.push_back(elf);
+            elf = 0;
+        }
     }
+    dataFile.close();
+    if (elf) {
+        elfs.push_back(elf);
+    }
+
+    sort(elfs.begin(), elfs.end(), descending);
+
+    // Part 1
+    cout << elfs[0] << endl;
+
+    // Part 2
+    cout << elfs[0] + elfs[1] + elfs[2] << endl;
+
+
     return 0;
 }
