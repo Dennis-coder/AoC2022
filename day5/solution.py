373fdb0f204413eb52b4a800bd0a5fba0bf81d89
Dennis Christensen <dennis.f.christensen@gmail.com>
2022-12-05 06:45:23 +0100

    cleaned day5 python

--- day5/solution.py
+++ day5/solution.py
@@ -12,37 +12,23 @@ def get_path():
 def parse():
     with open(get_path(), "r") as file:
         data = file.read().split("\n\n")
-    data[0] = data[0].split("\n")
-    stacks = []
-    for i in range(1, len(data[0][0]), 4):
-        stacks.append([])
-    for row in data[0][:-1][::-1]:
-        for i in range(1, len(row), 4):
-            if row[i] != " ":
-                stacks[i // 4].append(row[i])
-    data[0] = stacks
-    data[1] = [[int(x) for x in row.split() if x.isnumeric()] for row in data[1].split("\n")]
-    return data
+    stacks = [[row[i] for row in data[0].split("\n")[:-1][::-1] if row[i].isalpha()] for i in range(1, len(data[0].split("\n")[0]), 4)]
+    instructions = [[int(x) - (1 if i != 1 else 0) for i, x in enumerate(row.split()) if x.isnumeric()] for row in data[1].split("\n")]
+    return (stacks, instructions)
 
 def part1(data):
     stacks = deepcopy(data[0])
     for move, from_i, to_i in data[1]:
         for _ in range(move):
-            stacks[to_i - 1].append(stacks[from_i - 1].pop())
-    answer = ""
-    for stack in stacks:
-        answer += stack.pop()
-    return answer
+            stacks[to_i].append(stacks[from_i].pop())
+    return "".join([stack[-1] for stack in stacks])
 
 def part2(data):
     stacks = deepcopy(data[0])
     for move, from_i, to_i in data[1]:
-        stacks[to_i - 1] += stacks[from_i - 1][-move:]
-        stacks[from_i - 1] = stacks[from_i - 1][:-move]
-    answer = ""
-    for stack in stacks:
-        answer += stack.pop()
-    return answer
+        stacks[to_i] += stacks[from_i][-move:]
+        stacks[from_i] = stacks[from_i][:-move]
+    return "".join([stack[-1] for stack in stacks])
 
 
 if __name__ == "__main__":
