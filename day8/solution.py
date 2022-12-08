bbbcdfefefbf26f297e96b014318c252fe345fa4
Dennis Christensen <dennis.f.christensen@gmail.com>
2022-12-08 06:51:13 +0100

    day8 cleaned python

--- day8/solution.py
+++ day8/solution.py
@@ -15,18 +15,22 @@ def parse():
 
 def part1(data):
     visible = set()
-
     for y in range(len(data)):
         prev_h = -1
         for x in range(len(data[0])):
             if data[y][x] > prev_h:
                 visible.add((x,y))
             prev_h = max(prev_h, data[y][x])
+            if prev_h == 9:
+                break
+
         prev_h = -1
         for x in range(len(data[0]) - 1, -1, -1):
             if data[y][x] > prev_h:
                 visible.add((x,y))
             prev_h = max(prev_h, data[y][x])
+            if prev_h == 9:
+                break
 
     for x in range(len(data[0])):
         prev_h = -1
@@ -34,11 +38,16 @@ def part1(data):
             if data[y][x] > prev_h:
                 visible.add((x,y))
             prev_h = max(prev_h, data[y][x])
+            if prev_h == 9:
+                break
+
         prev_h = -1
         for y in range(len(data) - 1, -1, -1):
             if data[y][x] > prev_h:
                 visible.add((x,y))
             prev_h = max(prev_h, data[y][x])
+            if prev_h == 9:
+                break
     return len(visible)
 
 def part2(data):
@@ -46,23 +55,19 @@ def part2(data):
     for y in range(1, len(data) - 1):
         for x in range(1, len(data[0]) - 1):
             cur = data[y][x]
-            score = 1
-            for y2 in range(y - 1, -1, -1):
-                if data[y2][x] >= cur or y2 == 0:
-                    score *= abs(y - y2)
+            for y1 in range(y - 1, -1, -1):
+                if data[y1][x] >= cur:
                     break
             for y2 in range(y + 1, len(data)):
-                if data[y2][x] >= cur or y2 == len(data) - 1:
-                    score *= abs(y - y2)
+                if data[y2][x] >= cur:
                     break
-            for x2 in range(x - 1, -1, -1):
-                if data[y][x2] >= cur or x2 == 0:
-                    score *= abs(x - x2)
+            for x1 in range(x - 1, -1, -1):
+                if data[y][x1] >= cur:
                     break
             for x2 in range(x + 1, len(data[0])):
-                if data[y][x2] >= cur or x2 == len(data[0]) - 1:
-                    score *= abs(x - x2)
+                if data[y][x2] >= cur:
                     break
+            score = abs(x - x1) * abs(x - x2) * abs(y - y1) * abs(y - y2)
             best_score = max(best_score, score)
     
     return best_score
