4fb55b8fc70f380918384b8257d358badfef21be
Dennis Christensen <dennis.f.christensen@gmail.com>
2022-12-14 11:01:12 +0100

    day14 cleaned python

--- day14/solution.py
+++ day14/solution.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from copy import deepcopy
 
 
 def get_path():
@@ -11,10 +12,8 @@ def get_path():
 def parse():
     with open(get_path(), "r") as file:
         data = [[[int(coord) for coord in coords.split(",")] for coords in  row.split(" -> ")] for row in file.read().split("\n")]
-    return data
-
-def part1(data):
-    cave = {(500, 0): 0}
+    
+    cave = {}
     for path in data:
         start_x, start_y = path[0]
         for end_x, end_y in path[1:]:
@@ -27,12 +26,16 @@ def part1(data):
                 for x in range(min(start_x, end_x), max(start_x, end_x) + 1):
                     cave[(x, y)] = 1
             start_x, start_y = end_x, end_y
-    
+
+    return cave
+
+def part1(data):
+    cave = deepcopy(data)
     units_of_sand = 0
-    x1 = min([el[0] for el in cave])
-    x2 = max([el[0] for el in cave])
-    y1 = min([el[1] for el in cave])
-    y2 = max([el[1] for el in cave])
+    x1 = min([el[0] for el in cave] + [500])
+    x2 = max([el[0] for el in cave] + [500])
+    y1 = min([el[1] for el in cave] + [0])
+    y2 = max([el[1] for el in cave] + [0])
     while True:
         sand_x, sand_y = 500, 0
         while True:
@@ -59,20 +62,7 @@ def part1(data):
     return units_of_sand
 
 def part2(data):
-    cave = {(500, 0): 0}
-    for path in data:
-        start_x, start_y = path[0]
-        for end_x, end_y in path[1:]:
-            if start_x == end_x:
-                x = start_x
-                for y in range(min(start_y, end_y), max(start_y, end_y) + 1):
-                    cave[(x, y)] = 1
-            if start_y == end_y:
-                y = start_y
-                for x in range(min(start_x, end_x), max(start_x, end_x) + 1):
-                    cave[(x, y)] = 1
-            start_x, start_y = end_x, end_y
-    
+    cave = deepcopy(data)
     units_of_sand = 0
     floor = max([el[1] for el in cave]) + 2
     while True:
@@ -98,27 +88,7 @@ def part2(data):
         if sand_x == 500 and sand_y == 0:
             break
         
-
-    vizualize_cave(cave)
     return units_of_sand
-
-
-def vizualize_cave(cave):
-    x1 = min([el[0] for el in cave])
-    x2 = max([el[0] for el in cave])
-    y1 = min([el[1] for el in cave])
-    y2 = max([el[1] for el in cave])
-
-    for y in range(y1, y2 + 1):
-        row = ""
-        for x in range(x1, x2 + 1):
-            if (x, y) not in  cave:
-                row += " "
-            elif cave[(x, y)] == 1:
-                row += "#"
-            elif cave[(x, y)] == 2:
-                row += "o"
-        print(row)
 
 
 if __name__ == "__main__":
